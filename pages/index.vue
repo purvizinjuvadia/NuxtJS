64120f111608b97d9eef0c3c8b305991f654cd19
purvizinjuvadia <purvi.zinjuvadia@gmail.com>
2018-02-28 13:09:35 -0800

    part 2 : route with dynamic path and navigating around

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My stunning Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      <nuxt-link to="/users">Users</nuxt-link>
     </div>
   </section>
 </template>
---
+++ pages/users/_id/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>A single user, with ID: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+
+</script>
+
+<scope>
+
+</scope>
